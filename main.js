bc645a76bdb8ff41ab73dbbed6ba65378b801225
weirdwolfx <aadityapgoyal@gmail.com>
2025-07-14 20:09:25 +0530

    Add color pallete
    Colours blue, green, red, yellow, orange, white, random-color and darken-color

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-let squarePerSide = 16;
+// return random integer from 0 to n
+function random(n) {
+    return Math.floor(Math.random() * (n + 1));
+}
+
+// return a random color
+function getRandomColor() {
+    return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+}
+
+const squarePerSide = 16;
 
 const squareContainer = document.querySelector('.container');
+const randomColorPen = document.querySelector('#random');
+const darkenColorPen = document.querySelector('#darken');
 
-for (let i = 0; i < squarePerSide*squarePerSide; i++) {
-    const squareGrid = document.createElement("div");
-    squareGrid.classList.add("square")
+function makeGrid() {
+    for (let i = 0; i < squarePerSide * squarePerSide; i++) {
+        const squareGrid = document.createElement("div");
+        squareGrid.classList.add("square")
 
-    squareGrid.style.width = `${100 / squarePerSide}%`;
-    squareGrid.style.height = `${100 / squarePerSide}%`;
+        squareGrid.style.width = `${100 / squarePerSide}%`;
+        squareGrid.style.height = `${100 / squarePerSide}%`;
 
-    squareContainer.appendChild(squareGrid);
+        squareContainer.appendChild(squareGrid);
+    }
 }
+
+makeGrid();
 
