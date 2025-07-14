fe37670e8729f1163c951d6a48d25c3767058b99
weirdwolfx <aadityapgoyal@gmail.com>
2025-07-15 00:11:21 +0530

    Change brightness icon, and add pause feature
    User can now enable/disable the pen by pressing 'p' on keyboard

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ function random(n) {
     return Math.floor(Math.random() * (n + 1));
 }
 
+
 // return a random color
 function getRandomColor() {
     return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 }
 
+
+// number of squares per side
 const squarePerSide = 16;
 
 const squareContainer = document.querySelector('.container');
+const colorPallete = document.querySelector('.color-choices');
 const randomColorPen = document.querySelector('#random');
 const darkenColorPen = document.querySelector('#darken');
 
+
+// form a grid of side length = squarePerSide
 function makeGrid() {
     for (let i = 0; i < squarePerSide * squarePerSide; i++) {
         const squareGrid = document.createElement("div");
@@ -24,6 +30,43 @@ function makeGrid() {
 
         squareContainer.appendChild(squareGrid);
     }
+}
+
+
+// allow the user to draw on the grid
+function enablePen() {
+    squareContainer.addEventListener('mouseover', changeBoxColor);
+}
+
+
+function disablePen() {
+    squareContainer.removeEventListener('mouseover', changeBoxColor);
+}
+
+
+// allows user to pause/disable the pen by pressing the key 'p'
+function togglePen(e) {
+    if (e.key != 'p') return;
+
+    // if pen is enabled, grid has an attribute of 'pen'
+    if (squareContainer.hasAttribute('pen')) {
+        disablePen();
+    }
+    else {
+        enablePen();
+    }
+
+    // if pen was disabled, it will remove the attribute 'pen'...else it will add it
+    squareContainer.toggleAttribute('pen');
+}
+
+document.addEventListener('keypress', togglePen);
+
+
+// what to do when hovering over the grid squares
+function changeBoxColor(e) {
+    if (e.target.className != 'square') return;
+    console.log('I am stepping on box');
 }
 
 makeGrid();
